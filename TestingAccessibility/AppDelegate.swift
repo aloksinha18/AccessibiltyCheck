036de3e915f7fff853c136eabd60c8a746d77782
Alok <sinha18alok@gmail.com>
2020-09-06 19:11:59 +0200

    setup project

---
+++ TestingAccessibility/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  TestingAccessibility
+//
+//  Created by Alok Sinha on 2020-09-06.
+//  Copyright © 2020 Alok Sinha. All rights reserved.
+//
+
+import UIKit
+//Swift usage
+@UIApplicationMain
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ TestingAccessibility/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  TestingAccessibility
+//
+//  Created by Alok Sinha on 2020-09-06.
+//  Copyright © 2020 Alok Sinha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
